050adf04d311c36b100f7f674ea2a8e756643fde
apoc64 <sschwedt@gmail.com>
2018-07-25 10:15:42 -0600

    retreives data from oxford api

--- app/services/oxford_service.rb
+++ app/services/oxford_service.rb
@@ -4,18 +4,25 @@ class OxfordService
   end
 
   def examples
-    results
+    ex = results #[:lexicalEntries][:sentences]
     binding.pry
   end
 
   private
 
   def results
-    parse(get(word))
+    parse(get(@word))
   end
 
+  # May need to use lemmatron later
   def get(word)
-    Faraday.get("https://od-api.oxforddictionaries.com/api/v1/entries/#{word}&api_key=#{ENV["api_key"]}").body
+    conn = Faraday.new("https://od-api.oxforddictionaries.com:443/api/v1/entries/en/#{word}/sentences") do |faraday|
+      faraday.headers["Accept"] = "application/json"
+      faraday.headers["app_id"] = "858f22d1"
+      faraday.headers["app_key"] = "fe2c773b59e493eded930870360091a1"
+      faraday.adapter Faraday.default_adapter
+    end
+    conn.get.body
   end
 
   def parse(json)
