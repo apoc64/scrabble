d317d81c4dbccdde2befc60a0c5a3a61c37524b4
apoc64 <sschwedt@gmail.com>
2018-07-26 14:10:27 -0600

    moves api keys to Figaro

--- app/services/oxford_service.rb
+++ app/services/oxford_service.rb
@@ -21,8 +21,8 @@ class OxfordService
   def conn(word)
     Faraday.new("https://od-api.oxforddictionaries.com:443/api/v1/entries/en/#{word}/sentences") do |faraday|
       faraday.headers["Accept"] = "application/json"
-      faraday.headers["app_id"] = "858f22d1"
-      faraday.headers["app_key"] = "fe2c773b59e493eded930870360091a1"
+      faraday.headers["app_id"] = ENV["app_id"]
+      faraday.headers["app_key"] = ENV["app_keys"]
       faraday.adapter Faraday.default_adapter
     end
   end
